150c9cd86ec5f0aa746b6a1027f75368f07bc76b
gustavo-pimenta <63674712+gustavo-pimenta@users.noreply.github.com>
2020-06-18 15:51:18 -0300

    new file

---
+++ Automation PyAutoGUI Projects/shoot_bot.py
@@ -0,0 +1,26 @@
+import pyautogui
+import time
+
+pyautogui.FAILSAFE = True 
+
+def main():
+
+    while True:
+
+        mouse_pos=pyautogui.position()
+        if
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
