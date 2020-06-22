f699ac5ca2eccb60fbffcae3d71a7c7e82921d56
gustavo-pimenta <63674712+gustavo-pimenta@users.noreply.github.com>
2020-06-22 16:25:37 -0300

    new test

--- Automation Projects/shoot_bot/shoot_bot.py
+++ Automation Projects/shoot_bot/shoot_bot.py
@@ -1,43 +1,49 @@
-# import pyautogui
-from pynput import mouse
+from pynput.mouse import Listener
 import time
 
-# pyautogui.FAILSAFE = True 
+def on_move(x, y):
+    mouse_pos=(x, y)
+    return mouse_pos
+
+def on_click(x, y, button, pressed):
+    press=(x, y, button, pressed)
+    return press
+
+def on_scroll(x, y, dx, dy):
+    scroll=True
 
 
-# def on_click(x, y, button, pressed):
-#     x=('{0} at {1}'.format('Pressed' if pressed else 'Released',(x, y)))
-#     # Stop listener
-#     # if not pressed:
-#     #     return False
-
-# Collect events until released
-# with mouse.Listener(on_click=on_click,) as listener:
-#     listener.join()
-
-with mouse.Listener() as listener:
+with Listener(on_move=on_move, on_click=on_click, on_scroll=on_scroll) as listener:
+    print("oi")
     listener.join()
-
-listener.start()
-        
+    
 
 
-
-# def main():
-
-while True:
-
-    print(x)
-
-    with mouse.Events() as events:
-        for event in events:
-            if event.button == mouse.Button.left:
-                break
-            else:
-                print('Received event {}'.format(event))
-
+x=1
+while x==1:
+    print(mouse_pos)
     time.sleep(1)
 
-   
-# if __name__ == "__main__":
-#     main()
+
+
+
+
+
+
+# rom pynput import keyboard
+# import time
+
+# break_program = False
+# def on_press(key):
+#     global break_program
+#     print (key)
+#     if key == keyboard.Key.end:
+#         print ('end pressed')
+#         break_program = True
+#         return False
+
+# with keyboard.Listener(on_press=on_press) as listener:
+#     while break_program == False:
+#         print ('program running')
+#         time.sleep(5)
+#     listener.join()
